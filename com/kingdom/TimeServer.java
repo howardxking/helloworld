3e4359b4597c985f0c2a09044508b8810c28833a
aking <howardxking@qq.com>
2019-05-11 09:43:20 +0800

    nio case

--- com/kingdom/TimeServer.java
+++ com/kingdom/TimeServer.java
@@ -14,23 +14,8 @@ public class TimeServer {
 				// use default port number
 			}
 		}
-		ServerSocket server = null;
-		try {
-			server = new ServerSocket(port);
-			System.out.println("The time server is start in port: " + port);
-			Socket socket = null;
-			while(true) {
-				socket = server.accept();
-				new Thread(new TimeServerHandler(socket)).start();
-			}
-		}
-		finally {
-			if (null != server) {
-				System.out.println("The time server close");
-				server.close();
-				server = null;
-			}
-		}
+		MultiplexerTimeServer timeServer = new MultiplexerTimeServer(port);
+		new Thread(timeServer, "NIO-MultiplexerTimeServer-001").start();
 	}
 }
 
